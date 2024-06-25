c3e3df76e2ca140df2606b3b23ce78a190f32afc
2405-006-CW <2405-006-cw@U-2B5MB7QJAWMTD.corp.amazonworkspaces.com>
2024-06-25 15:31:41 +0100

    add p1 and p2 states and displays them in JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { DemonstratingProps } from "./components/demo/DemonstratingProps";
+import React from 'react'
 
 function App() {
+    const [player1Score, setPlayer1Score] = React.useState(0)
+    const [player2Score, setPlayer2Score] = React.useState(0)
     return (
         <div>
             <h1>App component</h1>
             <DemonstratingProps />
+            <h3>P1 score: {player1Score}</h3>
+            <h3>P2 score: {player2Score}</h3>
         </div>
     );
 }
