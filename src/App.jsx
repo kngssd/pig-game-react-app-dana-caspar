a81c1fce773635a6d6fa4d592ad312f70ae179c4
2405-006-CW <2405-006-cw@U-2B5MB7QJAWMTD.corp.amazonworkspaces.com>
2024-06-25 15:54:37 +0100

    add last roll state, and diesplay it in a h4 on a section

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import React from 'react'
 function App() {
     const [player1Score, setPlayer1Score] = React.useState(0)
     const [player2Score, setPlayer2Score] = React.useState(0)
+    const [lastDiceRoll, setLastDiceRoll] = React.useState('-')
     return (
         <div>
             <h1>App component</h1>
             <DemonstratingProps />
             <h3>P1 score: {player1Score}</h3>
             <h3>P2 score: {player2Score}</h3>
+            <section>
+                <h4>Last Roll: {lastDiceRoll}</h4>
+            </section>
         </div>
     );
 }
